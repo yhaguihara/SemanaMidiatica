8ab07d538621b57d40a142b443917c36482ea424
Marcio Marques <marciomojr.dev@gmail.com>
2026-09-14 20:50:23 -0300

    Adiciona html, css e js inicial

---
+++ script.js
@@ -0,0 +1,24 @@
+const track = document.getElementById('carouselTrack');
+
+const cards = [...track.children];
+
+cards.forEach(card => {
+    track.appendChild(card.cloneNode(true));
+});
+
+let posicao = 0;
+let larguraOriginal = track.scrollWidth / 2;
+
+function mover() {
+    posicao -= 0.5;
+
+    if (Math.abs(posicao) >= larguraOriginal) {
+        posicao = 0;
+    }
+
+    track.style.transform = `translateX(${posicao}px)`;
+
+    requestAnimationFrame(mover);
+}
+
+mover();
